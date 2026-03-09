ce1a2e64bc447218dc5a7b872114eff3bc9120cb
marcellusc27 <marcellus.mc@live.com>
2026-03-09 14:44:37 -0400

    Added Javadoc comments

---
+++ Obstacle.java
@@ -0,0 +1,52 @@
+package game;
+
+import java.awt.Graphics;
+
+abstract class Obstacle extends Polygon {
+	protected int[] xPts;
+	protected int[] yPts;
+	protected Point[] pts;
+
+	public Obstacle(Point[] inShape, Point inPosition, double inRotation) {
+		super(inShape, inPosition, inRotation);
+	}
+	
+	/**
+	 * changes how an obstacle moves either by rotating an element or shift position
+	 */
+	abstract public void update();
+	
+	/**
+	 * get method for x points
+	 * 
+	 * @return xPts an array of the x coordinates of the object
+	 */
+	public int[] getXPts() {
+		return xPts;
+	}
+	
+	/**
+	 * get method for x points
+	 * 
+	 * @return yPts an array of the y coordinates of the object
+	 */
+	public int[] getYPts() {
+		return yPts;
+	}
+
+	/**
+	 * Retrieves a list of points and initializes an array for xPts and yPts
+	 * to prepare for rendering
+	 * 
+	 * @param  brush 
+	 */
+	public void draw(Graphics brush) {
+        pts = getPoints();
+        xPts = new int[pts.length];
+        yPts = new int[pts.length];
+        for (int i = 0; i < pts.length; i++) {
+            xPts[i] = (int) pts[i].x;
+            yPts[i] = (int) pts[i].y;
+        }
+	}
+}
---
+++ Spike.java
@@ -0,0 +1,53 @@
+package game;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Spike extends Obstacle {
+	private double deltaX = 2;
+	private static final double MIN_X = 300;
+	private static final double MAX_X = 470;
+	private static final double MOVEMENT_SPEED = 2;
+
+	public Spike(int x, int y, int width, int height) {
+        super(new Point[]{
+            new Point(width / 2, 0), 
+            new Point(0, height),     
+            new Point(width, height)   
+        }, new Point(x, y), 0);
+    }
+	
+	/**
+     * Updates the object's horizontal position, reversing direction if it hits 
+     * the MIN_X value or MAX_X value.
+     */
+	public void update() {
+        if (position.x < MIN_X ) { 
+        	deltaX = MOVEMENT_SPEED; 
+        }
+        if (position.x > MAX_X) {
+        	deltaX = -MOVEMENT_SPEED;
+        }
+        position.x += deltaX;
+    }
+	
+	/**
+     * Draws the polygon onto the provided graphics context.
+     *
+     * This method first invokes the superclass draw logic, then renders the 
+     * polygon with a red fill and a black outline using the current 
+     * coordinate sets.
+     *
+     * @param brush the Graphics context used for drawing.
+     */
+	@Override
+	public void draw(Graphics brush) {
+        super.draw(brush);
+        
+        brush.setColor(Color.RED);
+        brush.fillPolygon(xPts, yPts, pts.length);
+        brush.setColor(Color.BLACK);
+        brush.drawPolygon(xPts, yPts, pts.length);
+    }
+
+}
